e0b8c409a2a5adb3f2554633ad20df4ff0c6ffa6
Megan Poosangtongchai <mpoosangtongchai@gmail.com>
2018-03-11 05:19:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,11 @@ for (let i = 0, l = musicians.length; i < l; i++) {
 }
 
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
 function johnLennonFacts

8534afc86359a30a21b0bba39c89fba634b19eac
Megan Poosangtongchai <mpoosangtongchai@gmail.com>
2018-03-11 05:05:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function theBeatlesPlay(musicians, instruments) {
   const arr = []
   
-  for(i = 0; i = musicians.length; i++ )
+  for(i = 0; l = musicians.length; i++) {
   console.log(name)
-}
+  }
+}  
+    for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
 
+  return arr
 }
